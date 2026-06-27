12641620ba3e8a3b835f27528c7eacdece2d53bf
Ron Shenkman <Ronshenkman16@gmail.com>
2026-06-27 21:16:04 +0300

    Fix serverless routing req.url mapping for Express on Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,21 @@
+const url = require('url');
 const app = require('../server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.query && parsedUrl.query.url) {
+        let targetUrl = parsedUrl.query.url;
+        
+        // Re-append other query parameters
+        const queryParams = { ...parsedUrl.query };
+        delete queryParams.url;
+        
+        const searchParams = new URLSearchParams(queryParams).toString();
+        if (searchParams) {
+            targetUrl += '?' + searchParams;
+        }
+        
+        req.url = targetUrl;
+    }
+    return app(req, res);
+};
